9025255d5dc493b668f405209ff88ab2815f388f
Luke VanderHart <luke@vanderhart.net>
2016-10-05 15:21:09 -0400

    add handler component for usability

--- src/arachne/http.clj
+++ src/arachne/http.clj
@@ -1,5 +1,6 @@
 (ns arachne.http
   (:require [arachne.core.util :as util]
+            [arachne.core.config :as cfg]
             [arachne.http.config :as http-cfg]
             [arachne.http.schema :as schema]
             [arachne.http.validators :as v]))
@@ -9,6 +10,23 @@
   can have dependencies, etc."
   (handle [this request] "Given a Ring-style request map, return a Ring-style
   response"))
+
+(defrecord HandlerComponent [handler dep-keys]
+  Handler
+  (handle [this request]
+    (handler (merge request (select-keys this dep-keys)))))
+
+(defn handler-component
+  "Constructor for a handler component"
+  [cfg eid]
+  (let [handler-kw (cfg/attr cfg eid :arachne.http.handler/fn)
+        dep-keys (cfg/q cfg '[:find [?key ...]
+                              :in $ ?component
+                              :where
+                              [?component :arachne.component/dependencies ?dep]
+                              [?dep :arachne.component.dependency/key ?key]] eid)
+        handler (util/require-and-resolve handler-kw)]
+    (->HandlerComponent handler dep-keys)))
 
 (defn schema
   "Return the schema for the arachne.http module"
