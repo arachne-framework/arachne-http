3dc77967382824685c9158994cd3c848099be10c
Luke VanderHart <luke@vanderhart.net>
2017-02-22 10:21:11 -0500

    add pure-Arachne url generator tool

--- src/arachne/http.clj
+++ src/arachne/http.clj
@@ -1,6 +1,10 @@
 (ns arachne.http
-  (:require [arachne.core.util :as util]
+  (:require [clojure.spec :as s]
+            [clojure.string :as str]
+            [arachne.core.util :as util]
             [arachne.core.config :as cfg]
+            [arachne.core.config.specs :as core-specs]
+            [arachne.error :refer [error deferror]]
             [arachne.http.config :as http-cfg]
             [arachne.http.schema :as schema]
             [arachne.http.validators :as v]))
@@ -17,7 +21,8 @@
     (handler (merge request (select-keys this dep-keys)))))
 
 (defn handler-component
-  "Constructor for a `Handler` component. The component must have a `:arachne.http.handler/fn` attribute indicating the handler function to which the request will be delegated."
+  "Constructor for a `Handler` component. The component must have a `:arachne.http.handler/fn`
+   attribute indicating the handler function to which the request will be delegated."
   [cfg eid]
   (let [handler-kw (cfg/attr cfg eid :arachne.http.handler/fn)
         dep-keys (cfg/q cfg '[:find [?key ...]
@@ -40,3 +45,91 @@
     (v/add-validators)
     (http-cfg/infer-endpoint-names)
     (http-cfg/add-endpoint-dependencies)))
+
+(s/fdef url-for
+  :args (s/cat :config ::core-specs/config
+               :server (s/or :entity-id ::core-specs/entity-id
+                             :arachne-id qualified-keyword?)
+               :endpoint keyword?
+               :params (s/? (s/map-of keyword? string? :min-count 1)))
+  :ret string?)
+
+(deferror ::urlgen-missing
+  :message  "Could not find endpoint named `:endpoint`"
+  :explanation "Attempted to generate a URL from the named endpoint `:endpoint`, but could not because no endpoint with that name could be found in the specified server `server`."
+  :suggestions ["Make sure the endpoint name is correct with no typos."]
+  :ex-data-docs {:endpoint "name of the endpoint"
+                 :server "the specified server in which to look"
+                 :cfg "the Arachne configuration"})
+
+(deferror ::urlgen-missing-param
+  :message  "Missing required URL parameter `:param` to generate url for endpoint named `:endpoint`."
+  :explanation "Attempted to generate a URL from the named endpoint `:endpoint`. However, the specified endpoint has a URL parameter or wildcard segment named `:param`, which was not specified in the given parameter map. Therefore, there is not enough data to generate a URL."
+  :suggestions ["Provide a value to use for `:param` in your call to `url-for`"]
+  :ex-data-docs {:endpoint "name of the endpoint"
+                 :param "the missing URL parameter or wildcard"
+                 :cfg "the Arachne configuration"})
+
+(defn- segment-string
+  "Return a function that, given a segment entity ID, returns an appropriate segment string"
+  [cfg params endpoint-name server]
+  (fn [{:keys [:arachne.http.route-segment/pattern
+               :arachne.http.route-segment/param
+               :arachne.http.route-segment/wildcard]}]
+    (cond
+      pattern pattern
+      wildcard (or (get params wildcard)
+                 (error ::urlgen-missing-param {:endpoint endpoint-name
+                                                :param wildcard
+                                                :cfg cfg}))
+      param (or (get params param)
+              (error ::urlgen-missing-param {:endpoint endpoint-name
+                                             :param param
+                                             :cfg cfg})))))
+
+(defn url-generator
+  "Given a configuration and a server (Arachne ID or entity ID), return a function that will
+   construct a query string given an endpoint name and an (optional) parameter map.
+
+   Usage example:
+
+       (def url-for (url-generator cfg :my/server))
+
+       (url-for :my-route {:n 42})
+       ;;=> \"/my/route/42\"
+
+   Returns a higher order function for performance reasons: calling `url-generator` performs an
+   expensive Datalog query of the configuration, but calls to the returned function are fast.
+   Therefore you should typically call `url-generator` once, and re-use the result."
+  [cfg server]
+  (let [server-ref (if (keyword? server) [:arachne/id server] server)
+        endpoints (cfg/q cfg '[:find ?name ?segment
+                               :in $ % ?server
+                               :where
+                               (endpoints ?server ?endpoint)
+                               [?endpoint :arachne.http.endpoint/name ?name]
+                               [?endpoint :arachne.http.endpoint/route ?segment]]
+                    http-cfg/route-rules server-ref)
+        endpoint-map (into {}
+                       (map (fn [[name segment]]
+                              (let [segment-eids (drop 1 (http-cfg/route-segments cfg segment))
+                                    segments (map #(cfg/pull cfg '[:arachne.http.route-segment/pattern
+                                                                   :arachne.http.route-segment/param
+                                                                   :arachne.http.route-segment/wildcard] %)
+                                               segment-eids)]
+                                [name segments]))
+                         endpoints))]
+    (fn url-for
+      ([endpoint-name] (url-for endpoint-name {}))
+      ([endpoint-name params]
+       (let [segments (get endpoint-map endpoint-name)]
+         (when-not segments
+           (error ::urlgen-missing {:endpoint endpoint-name
+                                    :server server
+                                    :cfg cfg}))
+         (->> segments
+           (map (segment-string cfg params endpoint-name server))
+           (str/join "/")
+           (str "/")))))))
+
+
---
+++ test/arachne/http_test.clj
@@ -0,0 +1,41 @@
+(ns arachne.http-test
+  (:require [clojure.test :refer :all]
+            [arachne.http.config :as http-cfg]
+            [arachne.core.config.validation :as v]
+            [arachne.core :as core]
+            [arachne.core.config :as cfg]
+            [arachne.core.config.script :as init]
+
+            [arachne.core.dsl :as a]
+            [arachne.http :as http]
+            [arachne.http.dsl :as h]
+            [arachne.http.dsl.test :as dsltest])
+  (:import (arachne ArachneException)))
+
+(defn url-gen-cfg []
+
+  (a/runtime :test/rt [:test/server])
+
+  (a/component :test/handler 'clojure.core/hash-map)
+
+  (dsltest/dummy-server :test/server 8080
+
+    (h/endpoint :get "/" :test/handler :name :a)
+    (h/endpoint :get "a/b/c" :test/handler :name :b)
+    (h/endpoint :get "a/:foo/:bar" :test/handler :name :c)
+    (h/endpoint :get "a/*wild" :test/handler :name :d)))
+
+(deftest url-gen
+  (let [cfg (core/build-config [:org.arachne-framework/arachne-http] `(url-gen-cfg))
+        url-for (http/url-generator cfg :test/server)]
+
+    (is (= "/" (url-for :a)))
+    (is (= "/a/b/c" (url-for :b)))
+    (is (= "/a/fred/bob" (url-for :c {:foo "fred" :bar "bob"})))
+    (is (= "/a/some/nested/url" (url-for :d {:wild "some/nested/url"})))
+
+    (is (thrown-with-msg? ArachneException #"Could not find endpoint"
+          (url-for :no-such-route)))
+
+    (is (thrown-with-msg? ArachneException #"Missing required URL parameter"
+          (url-for :c {:foo "fred"})))))
